1a6dcd3e03ae7ee2385f47e64d6497f624c35099
Genny Ocampo <gegex08@aol.com>
2016-02-08 13:01:06 -0600

    repo for the festival app

---
+++ GroupProjectFestivalApp/Infrastructure/FestivalRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GroupProjectFestivalApp.Infrastructure {
+    public class FestivalRepository: GenericRepository<> {
+        private ApplicationDbContext _db;
+
+        public FestivalRepository(DbContext db) {
+            _db = (ApplicationDbContext)db;
+        }
+        public IQueryable<T> List() {
+            return from e in Table
+                   where e.Active
+                   select e;
+        }
+    }
+}
