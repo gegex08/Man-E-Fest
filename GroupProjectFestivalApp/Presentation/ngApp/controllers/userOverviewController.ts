805ebfb4448a1c5a1652aff3f1394a0ee58e3603
AbrahamNino <abnino362@gmail.com>
2016-02-08 13:26:58 -0600

    ngController and WebApiController

---
+++ GroupProjectFestivalApp/Presentation/ngApp/controllers/userOverviewController.ts
@@ -0,0 +1,15 @@
+﻿namespace GroupProjectFestivalApp.Controllers {
+
+    export class OverViewController {
+
+        public overview;
+
+        constructor(private $http: ng.IHttpService, private $routeParams) {
+            $http.get(`/api/overview/${$routeParams.id}`)
+                .then((response) => {
+                    this.overview = response.data;
+                });
+
+        }
+    }
+}
