c16f5e2087e6bba9cc6d3190409efd302b73addf
brianvital <brianvital@hotmail.com>
2016-02-08 13:49:23 -0600

    models2

--- GroupProjectFestivalApp/Domain/Host.cs
+++ GroupProjectFestivalApp/Domain/Host.cs
@@ -1,9 +1,22 @@
-﻿using System;
+﻿using GroupProjectFestivalApp.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace GroupProjectFestivalApp.Domain {
     public class Host {
+
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public Pins {get;set;}
+
+    public Events {get; set;}
+
+public string Attractions { get; set; }
+
+public ApplicationUser User { get; set; }
     }
 }
