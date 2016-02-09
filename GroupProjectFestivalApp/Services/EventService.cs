924b5d90f18c2d93276f7e58797d8fc937612d3e
Genny Ocampo <gegex08@aol.com>
2016-02-09 15:05:19 -0600

    Event Services and DTO's

---
+++ GroupProjectFestivalApp/Services/EventService.cs
@@ -0,0 +1,31 @@
+﻿using GroupProjectFestivalApp.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProjectFestivalApp.Services {
+    public class EventService {
+        private EvenRepository _eventRepo;
+
+        public EventService(EventRepository eventRepo) {
+            _eventRepo = eventRepo;
+        }
+        public IList<EventDTO> GetEvent() {
+             
+            return (from e in _eventRepo.List()
+                    select new EventDTO() {
+
+                        Id = e.Id,
+                        StartTime = e.StartTime,
+                        EndTime = e.EndTime,
+                        Users = e.Users,
+                        Pushpins = e.Pushpins,
+                        Attractions = e.Attractions,
+                        Tags = e.Tags,
+                        Host = e.Host,
+                        Ratings = e.Ratings
+                    }).ToList();
+        }
+    }
+}
