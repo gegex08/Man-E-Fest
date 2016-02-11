6e6fda8f763d41a7b923f46b1e7aa139f3f864f1
AbrahamNino <abnino362@gmail.com>
2016-02-11 10:43:43 -0600

    tweeking html, not completed

--- GroupProjectFestivalApp/Services/Models/HostDTO.cs
+++ GroupProjectFestivalApp/Services/Models/HostDTO.cs
@@ -4,19 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace GroupProjectFestivalApp.Services.Models
-{
-    public class HostDTO
-    {
+namespace GroupProjectFestivalApp.Services.Models {
+    public class HostDTO {
 
         public int Id { get; set; }
         public string Url { get; set; }
         public string Name { get; set; }
         public IList<Pushpin> Pushpins { get; set; }
-        
+
         public IList<Event> Events { get; set; }
         public IList<Attraction> Attractions { get; set; }
         public ApplicationUser User { get; set; }
-                
+
     }
 }
