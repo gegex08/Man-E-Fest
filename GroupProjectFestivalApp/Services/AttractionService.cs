147848ead6dccd48eb3b9c7a20772095853ec756
AbrahamNino <abnino362@gmail.com>
2016-02-17 15:48:31 -0600

    added commentRepository, UserRepository, AttractionDTO, CommentService, AttractionServic\e

---
+++ GroupProjectFestivalApp/Services/AttractionService.cs
@@ -0,0 +1,27 @@
+﻿using GroupProjectFestivalApp.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProjectFestivalApp.Services.Models {
+    public class AttractionService {
+        private AttractionRepository _attractionRepo;
+
+        public AttractionService(AttractionRepository attractionRepo) {
+            _attractionRepo = attractionRepo;
+        }
+        public IList<AttractionDTO> GetAttraction() {
+            return (from a in _attractionRepo.List()
+                    select new AttractionDTO() {
+
+                        Name = a.Name,
+                        Description = a.Description,
+                        Rating = (int)a.Rating,
+                        PictureUrl = a.PictureUrl,
+                        SoundUrl = a.Sound
+                    }).ToList();
+        }
+    }
+}
---
+++ GroupProjectFestivalApp/Services/CommentService.cs
@@ -0,0 +1,45 @@
+﻿using GroupProjectFestivalApp.Infrastructure;
+using GroupProjectFestivalApp.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProjectFestivalApp.Services {
+    public class CommentService {
+        private CommentRepository _commentRepo;
+        private EventRepository _eventRepo;
+        private AttractionRepository _attractionRepo;
+
+        public CommentService(CommentRepository commentRepo, EventRepository eventRepo, AttractionRepository attractionRepo) {
+            _commentRepo = commentRepo;
+            _eventRepo = eventRepo;
+            _attractionRepo = attractionRepo;
+
+        }
+
+        public IList<CommentDTO> GetComment() {
+
+            return (from c in _commentRepo.List()
+                    select new CommentDTO() {
+
+                        Id = c.Id,
+                        Message = c.Message,
+                        Event = new EventDTO() {
+                            Id = c.Event.Id,
+                            Pushpin = c.Event.Pushpin.Name,
+                            Attraction = new AttractionDTO() {
+                                Name = c.Event.Attraction.Name,
+                            },
+                            Tags = (from t in c.Event.Tags
+                                    select t.Name).ToList(),
+                            Rating = (int)c.Event.Rating
+                        },
+                        User = new ApplicationUserDTO() {
+                            User = c.User.UserName
+                        }
+                    }).ToList();
+        }
+    }
+}
